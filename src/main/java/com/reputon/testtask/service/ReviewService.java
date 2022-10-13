a7d147f3043b063a4aa82f783ea4e0f61715292a
Nikita Parmon <nikita.parmon@q-perior.pl>
2022-10-13 13:08:21 +0200

    code refactoring

--- src/main/java/com/reputon/testtask/service/ReviewService.java
+++ src/main/java/com/reputon/testtask/service/ReviewService.java
@@ -1,44 +1,10 @@
 package com.reputon.testtask.service;
 
 import com.reputon.testtask.dto.ReviewDto;
-import com.reputon.testtask.exception.DomainNotFoundException;
-import com.reputon.testtask.parser.ReviewParser;
-
-import lombok.AllArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
+public interface ReviewService {
 
-@Service
-@AllArgsConstructor
-public class ReviewService {
-    private static final String TRUSTPILOT_URL = "https://www.trustpilot.com/review/";
-
-    private final WebClient client;
-    private final ReviewParser reviewParser;
-
-    @Cacheable(value = "domains")
-    public Mono<ResponseEntity<ReviewDto>> getReview(String domain) {
-        return client.get()
-                .uri(TRUSTPILOT_URL + domain)
-                .exchangeToMono(response -> {
-                    if (response.statusCode().is2xxSuccessful()) {
-                        return response.bodyToMono(DataBuffer.class)
-                                .map(buffer -> {
-                                    String string = buffer.toString(StandardCharsets.UTF_8);
-                                    DataBufferUtils.release(buffer);
-                                    ReviewDto result = reviewParser.parse(string);
-                                    return ResponseEntity.ok(result);
-                                });
-                    } else {
-                        return Mono.error(new DomainNotFoundException(domain));
-                    }
-                });
-    }
+    Mono<ResponseEntity<ReviewDto>> getReview(String domain);
 }
